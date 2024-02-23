eb1d214c72e30b4493a667c810a54282f23500ae
Dmitriy Pidko <pidkodima@gmail.com>
2024-02-23 16:33:32 +0200

    add random, add loading list

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom/client';
+import App from './components/app/App';
+// import MarvelService from './services/MarvelService'
+
+import './style/style.scss';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
